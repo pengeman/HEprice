e92f48297adcc0ad2618b87fdd810dfc1533bd90
pengeman <peng4567@163.com>
2023-05-23 18:19:01 +0800

    HEcompute db sample

--- DAO/HEdao.py
+++ DAO/HEdao.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://peng:sa@127.0.0.1:3306/heat_exchanger'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 # 获得所有板型数据
 def getSheetall():
+    sql = " select id , type, pic from u_sheet "
+    list_sheet = db.session.execute(sql)
+    for sheet in list_sheet:
+        print(sheet[0] + " - " + sheet[1])
 
-    pass
 # 获得板片数据by板型
 
 # 获得夹板数据all
@@ -20,5 +28,8 @@ def getSheetall():
 
 # 获得接管数据all
 
+
+if __name__ == '__main__':
+    getSheetall()
 
 #
